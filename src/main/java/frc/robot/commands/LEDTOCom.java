3550c8af22c8b4a91585e89173a45660d10ab8b0
acdelgado1019 <acdelgado1019@gmail.com>
2023-01-19 17:18:10 -0600

    NavX Vendor Library Updated, LED import correction

--- src/main/java/frc/robot/commands/LEDTOCom.java
+++ src/main/java/frc/robot/commands/LEDTOCom.java
@@ -1,11 +1,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.PlayerConfigs;
+import frc.robot.ControlConfigs.PlayerConfigs;
 import frc.robot.Robot;
 
 public class LEDTOCom extends CommandBase{
-    private boolean lockLed=false;
+    private boolean lockLed = false;
     
     public LEDTOCom(){
         addRequirements(Robot.ledStrip);
@@ -14,17 +14,15 @@ public class LEDTOCom extends CommandBase{
     @Override
     public void execute (){
         if (PlayerConfigs.signalCube){
-            lockLed= true;
+            lockLed = true;
             Robot.ledStrip.solid(150);
         } else if (PlayerConfigs.signalCone){
-            lockLed=true;
+            lockLed = true;
             Robot.ledStrip.solid(30);
         } else if (PlayerConfigs.toggleLeds){
-            lockLed= false;
-        }
-        else if (lockLed = false){
+            lockLed = false;
+        } else if (!lockLed){
             Robot.ledStrip.teamColor(Robot.teamColor); 
-
         }
     }
 }
