e8e8c6152af8c989973e24a482a97a12b1cd864c
Eftie <eppysilon@gmail.com>
2023-01-26 12:20:18 -0600

    What was left of my accidental crane wipe.

---
+++ src/main/java/frc/robot/subsystems/Crane.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Crane extends SubsystemBase {
+    private CANSparkMax claw;
+    private CANSparkMax rotator;
+    private CANSparkMax extender;
+
+    public RelativeEncoder clawEncoder;
+    public RelativeEncoder rotatorEncoder;
+    public RelativeEncoder extenderEncoder;
+    
+    
+}
