60714041b5e81f37d4df3c6872b4cca79d7ff147
acdelgado1019 <acdelgado1019@gmail.com>
2023-10-12 15:31:17 -0600

    Cell Names

--- src/main/java/frc/robot/subsystems/HDD/Cell.java
+++ src/main/java/frc/robot/subsystems/HDD/Cell.java
@@ -2,35 +2,21 @@ package frc.robot.subsystems.HDD;
 
 public class Cell {
     boolean cellSelected = false;
-    boolean cellScored = false;
-    double cranePosition = 0.0;
-    double wristPosition = 0.0;
-    double extenderPosition = 0.0;
+    String cellName;
     int ID;
 
-    public Cell(boolean enabled, int ID){
+    public Cell(boolean enabled, int ID, String Name){
         this.cellSelected = enabled;
         this.ID = ID;
+        this.cellName = Name;
     }
 
     public boolean getSelected(){
         return cellSelected;
     }
 
-    public boolean getScored(){
-        return cellScored;
-    }
-
-    public double getcranePosition(){
-        return cranePosition;
-    }
-
-    public double getwristPosition(){
-        return wristPosition;
-    }
-
-    public double getextenderPosition(){
-        return extenderPosition;
+    public String getName(){
+        return cellName;
     }
 
     public int getID(){
@@ -40,19 +26,4 @@ public class Cell {
     public void setSelected(boolean setpoint){
         cellSelected = setpoint;
     }
-
-    public void setScored(boolean setpoint){
-        cellScored = setpoint;
-    }
-    public void setcranePosition(double setpoint){
-        cranePosition = setpoint;
-    }
-
-    public void setwristPosition(double setpoint){
-        wristPosition = setpoint;
-    }
-    public void setextenderPosition(double setpoint){
-        extenderPosition = setpoint;
-    }
-
 }
--- src/main/java/frc/robot/subsystems/HDD/Grid.java
+++ src/main/java/frc/robot/subsystems/HDD/Grid.java
@@ -4,33 +4,33 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Grid {
     
-    public Cell H1 = new Cell(false, 31);
-    public Cell H2 = new Cell(false, 32);
-    public Cell H3 = new Cell(false, 33);
-    public Cell H4 = new Cell(false, 34);
-    public Cell H5 = new Cell(false, 35);
-    public Cell H6 = new Cell(false, 36);
-    public Cell H7 = new Cell(false, 37);
-    public Cell H8 = new Cell(false, 38);
-    public Cell H9 = new Cell(false, 39);
-    public Cell M1 = new Cell(false, 21);
-    public Cell M2 = new Cell(false, 22);
-    public Cell M3 = new Cell(false, 23);
-    public Cell M4 = new Cell(false, 24);
-    public Cell M5 = new Cell(false, 25);
-    public Cell M6 = new Cell(false, 26);
-    public Cell M7 = new Cell(false, 27);
-    public Cell M8 = new Cell(false, 28);
-    public Cell M9 = new Cell(false, 29);
-    public Cell L1 = new Cell(false, 11);
-    public Cell L2 = new Cell(false, 12);
-    public Cell L3 = new Cell(false, 13);
-    public Cell L4 = new Cell(false, 14);
-    public Cell L5 = new Cell(false, 15);
-    public Cell L6 = new Cell(false, 16);
-    public Cell L7 = new Cell(false, 17);
-    public Cell L8 = new Cell(false, 18);
-    public Cell L9 = new Cell(false, 19);
+    public Cell H1 = new Cell(false, 31, "H1");
+    public Cell H2 = new Cell(false, 32, "H2");
+    public Cell H3 = new Cell(false, 33, "H3");
+    public Cell H4 = new Cell(false, 34, "H4");
+    public Cell H5 = new Cell(false, 35, "H5");
+    public Cell H6 = new Cell(false, 36, "H6");
+    public Cell H7 = new Cell(false, 37, "H7");
+    public Cell H8 = new Cell(false, 38, "H8");
+    public Cell H9 = new Cell(false, 39, "H9");
+    public Cell M1 = new Cell(false, 21, "M1");
+    public Cell M2 = new Cell(false, 22, "M2");
+    public Cell M3 = new Cell(false, 23, "M3");
+    public Cell M4 = new Cell(false, 24, "M4");
+    public Cell M5 = new Cell(false, 25, "M5");
+    public Cell M6 = new Cell(false, 26, "M6");
+    public Cell M7 = new Cell(false, 27, "M7");
+    public Cell M8 = new Cell(false, 28, "M8");
+    public Cell M9 = new Cell(false, 29, "M9");
+    public Cell L1 = new Cell(false, 11, "L1");
+    public Cell L2 = new Cell(false, 12, "L2");
+    public Cell L3 = new Cell(false, 13, "L3");
+    public Cell L4 = new Cell(false, 14, "L4");
+    public Cell L5 = new Cell(false, 15, "L5");
+    public Cell L6 = new Cell(false, 16, "L6");
+    public Cell L7 = new Cell(false, 17, "L7");
+    public Cell L8 = new Cell(false, 18, "L8");
+    public Cell L9 = new Cell(false, 19, "L9");
 
     public void updateCells(){
         H1.setSelected(SmartDashboard.getBoolean("H1", false));
@@ -125,6 +125,7 @@ public class Grid {
     public void clearPrevSelect(Cell prevID){
         if(prevID.getID() != getSelectedCell().getID()){
             prevID.setSelected(false);
+            SmartDashboard.putBoolean(getSelectedCell().getName(), false);
         }
     }
 
